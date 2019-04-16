c9395eb122513a5b8c5bf23ba1b4b793c8e1f80c
andro1010ag <andresgarcia17@icloud.com>
2019-04-15 20:16:32 -0500

    HW3

---
+++ Assignments/HW3_JavaPrimitives/ecuacion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package raices;
+
+/**
+ *
+ * @author andresgarcia
+ */
+public class ecuacion {
+    public static void main(String[] args){
+        Raices ecuacion= new Raices(3,4,4);
+        ecuacion.calculate();
+    }
+}
