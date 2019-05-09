a5c1f792c53689774e3b842d1880e2ba7834699a
andro1010ag <andresgarcia17@icloud.com>
2019-05-08 19:03:26 -0500

    Lab8

---
+++ Labs/Lab8_Polymorphism&javadoc/src/ec/edu/espe/lab7fundamentals/oopfundamentals/fundamentals.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.lab7fundamentals.oopfundamentals;
+import ec.edu.espe.lab7fundamentals.oopfundamentals.inheritance.Animal;
+import ec.edu.espe.lab7fundamentals.oopfundamentals.inheritance.Lion;
+import ec.edu.espe.lab7fundamentals.oopfundamentals.inheritance.Mammal;
+
+/**
+ *
+ * @author Andres Garcia
+ */
+public class fundamentals {
+    public static void main( String[] args ){
+        
+     
+        
+//        Mammal mammal = new Mammal(2,4, "Firulais") {};
+//        mammal.print();
+        
+        Animal animal = new Lion(true, true, 3, 4, "Alex");
+        System.out.println(animal.getData());
+    }
+}
---
+++ Labs/Lab8_Polymorphism&javadoc/src/ec/edu/espe/lab7fundamentals/oopfundamentals/inheritance/Tiger.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.lab7fundamentals.oopfundamentals.inheritance;
+
+/**
+ *
+ * @author Andres Garcia
+ */
+public class Tiger extends Feline {
+
+    public Tiger(boolean scary, int furThickness, int numberOfLegs, String name) {
+        super(scary, furThickness, numberOfLegs, name);
+    }
+
+    @Override
+    public String getData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+  
+
+}
